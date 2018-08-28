69971d430fb8fa6313868bdc0423995730efaca2
lbrobb <lbrobb@gmail.com>
2018-08-28 23:30:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function fetchBooks() {
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
+    const h2 = document.createElement('h4')
+    h4.innerHTML = `<h4>${book.name}</h4>`
     main.appendChild(h2)
   })
 }
