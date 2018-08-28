d007d724c8cfd4a4cfb879ff5e6eb7641358c840
lbrobb <lbrobb@gmail.com>
2018-08-28 23:14:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 // starter code
 
-function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
-    .then(resp => resp.json())
-    .then(json => console.log(json));
-}
-
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+// function fetchBooks() {
+//   fetch('https://anapioficeandfire.com/api/books')
+//     .then(resp => resp.json())
+//     .then(json => console.log(json));
+// }
+// 
+// function renderBooks(json) {
+//   const main = document.querySelector('main')
+//   json.forEach(book => {
+//     const h2 = document.createElement('h2')
+//     h2.innerHTML = `<h2>${book.name}</h2>`
+//     main.appendChild(h2)
+//   })
+// }
+// 
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks()
+// })
 
 // parse json
 
